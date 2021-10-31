c9d812fd4dfb9d3ae64d368e86fbae290098a378
Adam B <adam@bielas.pl>
2021-10-31 15:05:35 +0100

    Dodaj pliki projektów.

---
+++ Bielas.pl - Windows Doktor/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Bielas.pl___Windows_Doktor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Opis = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Kategoria,
+            this.Opis});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(462, 450);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 11;
+            this.listBox1.Location = new System.Drawing.Point(3, 278);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(328, 169);
+            this.listBox1.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 462;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(47, 206);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(334, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.UseWaitCursor = true;
+            this.progressBar1.Visible = false;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 30;
+            // 
+            // Kategoria
+            // 
+            this.Kategoria.HeaderText = "Kategoria";
+            this.Kategoria.Name = "Kategoria";
+            this.Kategoria.ReadOnly = true;
+            // 
+            // Opis
+            // 
+            this.Opis.HeaderText = "Opis";
+            this.Opis.Name = "Opis";
+            this.Opis.ReadOnly = true;
+            this.Opis.Width = 300;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Bielas.pl - Windows Doktor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Opis;
+    }
+}
+
---
+++ Bielas.pl - Windows Doktor/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bielas.pl___Windows_Doktor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.dataGridView1.Rows.Add("1", "Sieć", "ping 1.1.1.1 -t");
+            this.dataGridView1.Rows.Add("2", "Sieć", "tracert 1.1.1.1");
+            this.dataGridView1.Rows.Add("3", "Sieć", "ipconfig /all");
+            this.dataGridView1.Rows.Add("4", "Sieć", "ipconfig /renew");
+            this.dataGridView1.Rows.Add("999", "Windows", "Szybka Pomoc");
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Thread t1 = new Thread(start);
+            //t1.Start();
+            progressBar1.Visible = true;
+            dataGridView1.Enabled = false;
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = runspace;
+            switch (Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value))
+            {
+                case 1: System.Diagnostics.Process.Start("cmd.exe", "/K ping 1.1.1.1 -t"); break;
+                case 2: System.Diagnostics.Process.Start("cmd.exe", "/K tracert 1.1.1.1"); break;
+                case 3: System.Diagnostics.Process.Start("cmd.exe", "/K ipconfig /all"); break;
+                case 4: System.Diagnostics.Process.Start("cmd.exe", "/K ipconfig /renew"); break;
+
+
+
+                case 999: System.Diagnostics.Process.Start("quickassist.exe", ""); break;
+                default:
+                    MessageBox.Show("No i popsułeś!");
+                    break;
+
+
+            }
+            //Collection<PSObject> results = ps.Invoke();
+            //runspace.Close();
+            //StringBuilder stringBuilder = new StringBuilder();
+            //listBox1.Items.Clear();
+            //foreach (PSObject obj in results) listBox1.Items.Add(obj.ToString());
+            Thread t2 = new Thread(stop);
+            t2.Start();
+
+        }
+    
+        public void start()
+        {
+            progressBar1.Invoke(new Action(delegate(){progressBar1.Visible = true;}));
+            dataGridView1.Invoke(new Action(delegate(){dataGridView1.Enabled = false;}));            
+        }
+        void stop()
+        {
+            progressBar1.Invoke(new Action(delegate () { progressBar1.Visible = false; }));
+            dataGridView1.Invoke(new Action(delegate () { dataGridView1.Enabled = true; }));
+        }
+    }
+}
